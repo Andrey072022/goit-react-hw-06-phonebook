8048248a1cd0b38111e729c80ad78654d0ee7b57
Svyntsov Andriy <svincov_a@ukr.net>
2023-08-15 23:15:17 +0300

    15.08

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
+import React from 'react';
+import ContactForm from 'components/ContactForm';
+import Filter from 'components/Filter';
+import ContactList from 'components/ContactList';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </>
   );
 };
+
+export default App;
